8bacecec11f4f6be692b9b4a0dcbb2f3481a35d6
fabian <fg60hyfy@studserv.uni-leipzig.de>
2019-05-08 18:04:59 +0200

    CLI vergessen

---
+++ src/main/scala/CLI.scala
@@ -0,0 +1,24 @@
+import org.rogach.scallop._
+
+
+
+class CLIConf(arguments: Seq[String]) extends ScallopConf(arguments) {
+
+  banner("""
+
+Example: scala main.scala -q "/dbpediaclient/src/query/downloadquery  --repo "/dbpediaclient/converted_files/" -o "GZIP2"
+
+For usage see below:
+    """)
+
+  val query = opt[String]("query", required = true)
+  val localrepo = opt[String]("localrepo", default= Some("./converted_files/"), descr = "set the destination dir for converted files")
+  val outputCompression = opt[String]("outCompression", default= Some("bzip2"), descr = "set the Compression you want the Output File to have")
+  val help = opt[Boolean]("help", noshort = true, descr = "Show this message")
+
+  verify()
+
+}
+
+
+
