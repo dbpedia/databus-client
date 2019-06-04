ed47efadc47c020a2409a5ff20d7d6561f57a31f
fabian <fg60hyfy@studserv.uni-leipzig.de>
2019-06-04 16:05:18 +0200

    Dataid Querys forgotten

---
+++ src/main/scala/org/dbpedia/databus/sparql/DataId.scala
@@ -0,0 +1,40 @@
+package org.dbpedia.databus.sparql
+
+object DataIdQueries {
+
+  def queryGetPublisher(): String =
+    s"""
+       |PREFIX dataid: <http://dataid.dbpedia.org/ns/core#>
+       |SELECT ?o {
+       |  ?dataset a dataid:Dataset;
+       |           dataid:account ?o .
+       |}
+    """.stripMargin
+
+  def queryGetGroup(): String =
+    s"""
+       |PREFIX dataid: <http://dataid.dbpedia.org/ns/core#>
+       |SELECT ?o {
+       |  ?dataset a dataid:Dataset;
+       |           dataid:group ?o .
+       |}
+    """.stripMargin
+
+  def queryGetArtifact(): String =
+    s"""
+       |PREFIX dataid: <http://dataid.dbpedia.org/ns/core#>
+       |SELECT ?o {
+       |  ?dataset a dataid:Dataset;
+       |           dataid:artifact ?o .
+       |}
+    """.stripMargin
+
+  def queryGetVersion(): String =
+    s"""
+       |PREFIX dataid: <http://dataid.dbpedia.org/ns/core#>
+       |SELECT ?o {
+       |  ?dataset a dataid:Dataset;
+       |           dataid:version ?o .
+       |}
+    """.stripMargin
+}
