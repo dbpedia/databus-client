dda8c48008ce597b174513d0934be6b5e0d5b9e4
fabian <fg60hyfy@studserv.uni-leipzig.de>
2019-06-14 12:49:47 +0200

    Dateien können nun auch ohne Kompression abgespeichert werden.

--- src/main/scala/org/dbpedia/databus/Main.scala
+++ src/main/scala/org/dbpedia/databus/Main.scala
@@ -9,28 +9,34 @@ object Main {
     val conf = new CLIConf(args)
     val dir_download = "./downloaded_files/"
 
-//    //Test if query is a File or a Query
-//    var queryString:String = ""
-//    File(conf.query()).exists() match {
-//      case true => {
-//        // "./src/query/query"
-//        val file = File(conf.query())
-//        queryString = FileHandler.readQueryFile(file)
-//      }
-//      case false => {
-//        queryString = conf.query()
-//      }
-//    }
-//    println(queryString)
-//    QueryHandler.executeSelectQuery(queryString)
+    //Test if query is a File or a Query
+    var queryString:String = ""
+    File(conf.query()).exists() match {
+      case true => {
+        // "./src/query/query"
+        val file = File(conf.query())
+        queryString = FileHandler.readQueryFile(file)
+      }
+      case false => {
+        queryString = conf.query()
+      }
+    }
+    println(queryString)
+    QueryHandler.executeSelectQuery(queryString)
+
+//  if "no" compression wanted change the value to an empty string
+    var outputCompression = conf.outputCompression()
+    if (conf.outputCompression()=="no") {
+      outputCompression=""
+    }
 
     var dir = File(dir_download)
     var files = dir.listRecursively.toSeq
     for (file <- files) {
         if (! file.isDirectory){
           if (!file.name.equals("dataid.ttl")){
-            println(s"Konvertiere: ${file.pathAsString} zu ${conf.outputFormat()}.${conf.outputCompression()}")
-            FileHandler.convertFile(file, conf.localrepo(), conf.outputFormat(), conf.outputCompression() )
+            println(s"Konvertiere: ${file.pathAsString} zu ${conf.outputFormat()}.${outputCompression}")
+            FileHandler.convertFile(file, conf.localrepo(), conf.outputFormat(), outputCompression )
           }
         }
     }
