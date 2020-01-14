b31e90637c8d5b967eb3e249dc065472983c619c
Eisenbahnplatte <18533014+Eisenbahnplatte@users.noreply.github.com>
2020-01-14 21:03:01 +0100

    updated docker and example queries

--- docker/virtuoso-image/entrypoint.sh
+++ docker/virtuoso-image/entrypoint.sh
@@ -13,6 +13,10 @@ if { [ -z "$SOURCE" ] && [ ! -z "$S" ]; } || { [ ! -z "$SOURCE" ] && [ -z "$S" ]
     args="$args|--source|$SOURCE$S"
 fi
 
+if { [ -z "$FORMAT" ] && [ ! -z "$F" ]; } || { [ ! -z "$FORMAT" ] && [ -z "$F" ]; }; then
+    args="$args|--format|$FORMAT$F"
+fi
+
 mvn -q scala:run -Dlauncher="databusclient" -DaddArgs="$args"
 
 mv -t $LOAD $(find $TARGET -name "*.gz")
