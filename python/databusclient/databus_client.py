5f4a0c8b5b64ff1de4fb0c2342c43abc5acbe1b2
Denis <yum-yab-dev@pm.me>
2022-01-14 13:04:22 +0100

    Adds cmdtool and the lib file

--- python/databusclient/databus_client.py
+++ python/databusclient/databus_client.py
@@ -1,300 +1,292 @@
-import requests
-import json
-import hashlib
-import sys
-from datetime import datetime
-from dataclasses import dataclass, field
-from typing import List
-
-DATABUS_URI_BASE = "http://localhost:3000"
-
-@dataclass
-class DataGroup:
-    account_name: str
-    id: str
-    label: str
-    title: str
-    comment: str
-    abstract: str
-    description: str
-    context: str = "https://raw.githubusercontent.com/dbpedia/databus-git-mockup/main/dev/context.jsonld"
-
-    def get_target_uri(self) -> str:
-
-        return f"{DATABUS_URI_BASE}/{self.account_name}/{self.id}"
-
-    def to_jsonld(self) -> str:
-        """Generates the json representation of group documentation"""
-
-        group_uri = f"{DATABUS_URI_BASE}/{self.account_name}/{self.id}"
-
-        group_data_dict = {
-            "@context": self.context,
-            "@graph": [
-                {
-                    "@id": group_uri,
-                    "@type": "dataid:Group",
-                    "label": {"@value": self.label, "@language": "en"},
-                    "title": {"@value": self.title, "@language": "en"},
-                    "comment": {"@value": self.comment, "@language": "en"},
-                    "abstract": {"@value": self.abstract, "@language": "en"},
-                    "description": {"@value": self.description, "@language": "en"},
-                }
-            ],
-        }
-        return json.dumps(group_data_dict)
+import databus_client_lib
+import argparse_prompt
 
 
-class DatabusFile:
-    def __init__(self, uri: str, cvs: dict, file_ext: str, **kwargs):
-        """Fetches the necessary information of a file URI for the deploy to the databus."""
-        self.uri = uri
-        self.cvs = cvs
-        resp = requests.get(uri, **kwargs)
-        if resp.status_code > 400:
-            print(f"ERROR for {uri} -> Status {str(resp.status_code)}")
+def direct_group_deploy(args):
 
-        self.sha256sum = hashlib.sha256(bytes(resp.content)).hexdigest()
-        self.content_length = str(len(resp.content))
-        self.file_ext = file_ext
-        self.id_string = "_".join([f"{k}={v}" for k, v in cvs.items()]) + "." + file_ext
+    group = databus_client_lib.DataGroup(
+        args.user,
+        accountargs.group,
+        args.title,
+        args.title,
+        args.comment,
+        args.documentation,
+        args.documentation,
+        DATABUS_BASE=args.base,
+    )
 
-
-@dataclass
-class DataVersion:
-    account_name: str
-    group: str
-    artifact: str
-    version: str
-    title: str
-    label: str
-    publisher: str
-    comment: str
-    abstract: str
-    description: str
-    license: str
-    databus_files: List[DatabusFile]
-    issued: datetime = field(default_factory=datetime.now)
-    context: str = "https://raw.githubusercontent.com/dbpedia/databus-git-mockup/main/dev/context.jsonld"
-
-    def get_target_uri(self):
-
-        return f"{DATABUS_URI_BASE}/{self.account_name}/{self.group}/{self.artifact}/{self.version}"
-
-    def __distinct_cvs(self) -> dict:
-
-        distinct_cv_definitions = {}
-        for dbfile in self.databus_files:
-            for key, value in dbfile.cvs.items():
-
-                if not key in distinct_cv_definitions:
-                    distinct_cv_definitions[key] = {
-                        "@type": "rdf:Property",
-                        "@id": f"dataid-cv:{key}",
-                        "rdfs:subPropertyOf": {"@id": "dataid:contentVariant"},
-                    }
-        return distinct_cv_definitions
-
-    def __dbfiles_to_dict(self):
-
-        for dbfile in self.databus_files:
-            file_dst = {
-                "@id": self.version_uri + "#" + dbfile.id_string,
-                "file": self.version_uri + "/" + self.artifact + "_" + dbfile.id_string,
-                "@type": "dataid:SingleFile",
-                "formatExtension": dbfile.file_ext,
-                "compression": "none",
-                "downloadURL": dbfile.uri,
-                "byteSize": dbfile.content_length,
-                "sha256sum": dbfile.sha256sum,
-                "hasVersion": self.version,
-            }
-            for key, value in dbfile.cvs.items():
-
-                file_dst[f"dataid-cv:{key}"] = value
-
-            yield file_dst
-
-    def to_jsonld(self, **kwargs) -> str:
-        self.version_uri = (
-            f"{DATABUS_URI_BASE}/{account_name}/{group}/{artifact}/{version}"
-        )
-        self.data_id_uri = self.version_uri + "#Dataset"
-
-        self.artifact_uri = (
-            f"{DATABUS_URI_BASE}/{account_name}/{group}/{artifact}"
-        )
-
-        self.group_uri = f"{DATABUS_URI_BASE}/{account_name}/{group}"
-
-        self.timestamp = self.issued.strftime("%Y-%m-%dT%H:%M:%SZ")
-
-        data_id_dict = {
-            "@context": self.context,
-            "@graph": [
-                {
-                    "@type": "dataid:Dataset",
-                    "@id": self.data_id_uri,
-                    "version": self.version_uri,
-                    "artifact": self.artifact_uri,
-                    "group": self.group_uri,
-                    "hasVersion": self.version,
-                    "issued": self.timestamp,
-                    "publisher": self.publisher,
-                    "label": {"@value": self.label, "@language": "en"},
-                    "title": {"@value": self.title, "@language": "en"},
-                    "comment": {"@value": self.comment, "@language": "en"},
-                    "abstract": {"@value": self.abstract, "@language": "en"},
-                    "description": {"@value": self.description, "@language": "en"},
-                    "license": {"@id": self.license},
-                    "distribution": [d for d in self.__dbfiles_to_dict()],
-                }
-            ],
-        }
-
-        for _, named_cv_prop in self.__distinct_cvs().items():
-            data_id_dict["@graph"].append(named_cv_prop)
-
-        # add explicit artifact statement
-
-        data_id_dict["@graph"].append({"@id": self.get_target_uri().rsplit("/", 1)[0], "@type": "dataid:Artifact"})
-
-        # Explicit Version Statement
-        data_id_dict["@graph"].append({"@id": self.get_target_uri(), "@type": "dataid:Version"})
+    databus_client_lib.deploy_to_dev_databus(args.apikey, group)
 
 
-        return json.dumps(data_id_dict)
+def generate_group(args):
+
+    group = databus_client_lib.DataGroup(
+        args.user,
+        args.group,
+        args.title,
+        args.title,
+        args.comment,
+        args.documentation,
+        args.documentation,
+        DATABUS_BASE=args.base,
+    )
+
+    if args.file is not None and args.file != "":
+        fname = args.file
+    else:
+        fname = "group.jsonld"
+
+    with open(fname, "w+") as f:
+        print(group.to_jsonld(indent=2), file=f)
 
 
-def deploy_to_databus(user: str, passwd: str, *databus_objects):
-    try:
-        # Request data for bearer token
-        data = {
-            "client_id": "upload-api",
-            "username": user,
-            "password": passwd,
-            "grant_type": "password",
-        }
+def generate_version(args):
 
-        # requesting the bearer token and saving it in a variable
-        print("Accessing new token...")
-        token_response = requests.post(
-            "https://databus.dbpedia.org/auth/realms/databus/protocol/openid-connect/token",
-            data=data,
-        )
-        print(f"Response: Status {token_response.status_code}")
+    dbfiles = []
 
-        token = token_response.json()["access_token"]
+    for uri in args.URIs:
+        dbfiles.append(databus_client_lib.DatabusFile(uri, {}, "file"))
 
-    except Exception as e:
-        print(f"Error requesting token: {str(e)}")
-        sys.exit(1)
+    version = databus_client_lib.DataVersion(
+        args.user,
+        args.group,
+        args.artifact,
+        args.version,
+        args.title,
+        args.title,
+        args.publisher,
+        args.comment,
+        args.documentation,
+        args.documentation,
+        args.license,
+        dbfiles,
+        DATABUS_BASE=args.base,
+    )
+    if args.file is not None and args.file != "":
+        fname = args.file
+    else:
+        fname = "version.jsonld"
 
-    for dbobj in databus_objects:
-        # send the dataid as JSON-LD to the target
-        # The Authorisation header must be set with "Bearer $TOKEN"
-        # https://databus.dbpedia.org/account/group for group metadata
-        # https://databus.dbpedia.org/account/group/artifact/version for Databus version
-        headers = {"Authorization": "Bearer " + token}
-
-        print(f"Deploying {dbobj.get_target_uri()}")
-        response = requests.put(
-            dbobj.get_target_uri(), headers=headers, data=dbobj.to_jsonld()
-        )
-        print(f"Response: Status {response.status_code}; Text: {response.text}")
+    with open(fname, "w+") as f:
+        print(version.to_jsonld(indent=2), file=f)
 
 
-def deploy_to_dev_databus(api_key: str, *databus_objects):
+def direct_version_deploy(args):
 
-    for dbobj in databus_objects:
-        print(f"Deploying {dbobj.get_target_uri()}")
-        submission_data = dbobj.to_jsonld()
+    dbfiles = []
 
-        resp = requests.put(dbobj.get_target_uri(), headers={"X-API-Key": api_key, "Content-Type": "application/json"}, data=submission_data)
+    for uri in args.URIs:
+        dbfiles.append(databus_client_lib.DatabusFile(uri, {}, "file"))
 
-        if resp.status_code >= 400:
-            print(f"Response: Status {resp.status_code}; Text: {resp.text}")
+    version = databus_client_lib.DataVersion(
+        args.user,
+        args.group,
+        args.artifact,
+        args.version,
+        args.title,
+        args.title,
+        args.publisher,
+        args.comment,
+        args.documentation,
+        args.documentation,
+        args.license,
+        dbfiles,
+        DATABUS_BASE=args.base,
+    )
 
-            print(f"Problematic file:\n {submission_data}")
-
-
-def main():
-    print("DATABUS CLIENT v0.1")
-    print("-------------------")
+    databus_client_lib.deploy_to_dev_databus(args.apikey, version)
 
 
 if __name__ == "__main__":
 
-    account_name = "vehnem"
+    parser = argparse_prompt.PromptParser()
 
-    group = "general"
+    subparsers = parser.add_subparsers(dest='command', help="sub-command help")
 
-    artifact = "testartifact"
+    # Some parameters you always need
 
-    version = "2022-01-13"
-
-    title = "Test Title"
-
-
-    # currently its advised to use the internal webid found at https://dev.databus.dbpedia.org/{{user}}#settings
-    publisher = "http://localhost:3000/vehnem#this"
-
-    label = "Test Label"
-
-    comment = "This is a short comment about the test."
-
-    abstract = "This a short abstract for the dataset. Since this is only a test it is quite insignificant."
-
-    description = "A bit longer description of the dataset."
-
-    license = "http://this.is.a.license.uri.com/test"
-
-    files = [
-        DatabusFile(
-            "https://yum-yab.github.io/data/databus-api-test/first/pizza-ont.owl",
-            {"type": "ontology"},
-            "owl",
-        ),
-        DatabusFile(
-            "https://yum-yab.github.io/data/databus-api-test/first/Sample500.csv",
-            {"type": "randomData"},
-            "csv",
-        ),
-        DatabusFile(
-            "https://openenergy-platform.org/api/v0/schema/supply/tables/wind_turbine_library/rows/",
-            {"type": "turbineData", "extra": "external"},
-            "json",
-        ),
-    ]
-
-    databus_version = DataVersion(
-        account_name=account_name,
-        group=group,
-        artifact=artifact,
-        version=version,
-        title=title,
-        publisher=publisher,
-        label=label,
-        comment=comment,
-        abstract=abstract,
-        description=description,
-        license=license,
-        databus_files=files,
+    parser.add_argument(
+        "--verbose",
+        "-v",
+        help="Prints out steps and generated content",
+        action="store_true",
+        prompt=False,
     )
 
-    databus_group = DataGroup(
-        account_name=account_name,
-        id=group,
-        label="Test Group",
-        title="Test Group",
-        abstract="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
-        comment="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
-        description="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.",
+    parser.add_argument(
+        "--base",
+        "-b",
+        help="The base for the Databus. Default is https://dev.databus.dbpedia.org",
+        default="https://dev.databus.dbpedia.org",
+        prompt=False,
     )
 
-    # for the current version of the databus
-    # deploy_to_databus(account_name, "passwort", databus_group, databus_version)
+    # first do the generate parsers for both groups and Versions
+    generate_parser = subparsers.add_parser("generate", help="generate help")
 
-    # For the new version deployed to dev.databus.dbpedia.org
-    # API KEY can be found or generated under https://dev.databus.dbpedia.org/{{user}}#settings
-    deploy_to_dev_databus("035d682f-767c-47ad-aa0c-f6731b6ce21d", databus_group, databus_version)
+    generate_subparsers = generate_parser.add_subparsers(dest='type', help="type help")
+
+    generate_parser.add_argument(
+        "--file", "-f", help="The file the result should be printed to.", type=str
+    )
+
+    # parser for generating group
+
+    group_generate_parser = generate_subparsers.add_parser("group", help="group help")
+
+    group_generate_parser.add_argument(
+        "--user", "-u", help="The databus user", type=str
+    )
+
+    group_generate_parser.add_argument("--group", "-g", help="The group name", type=str)
+
+    group_generate_parser.add_argument(
+        "--title", "-t", help="The group title", type=str
+    )
+
+    group_generate_parser.add_argument(
+        "--comment", "-c", help="The group comment", type=str
+    )
+
+    group_generate_parser.add_argument(
+        "--documentation", "-doc", help="The group documentation", type=str
+    )
+
+    # parser for generating version
+
+    version_generate_parser = generate_subparsers.add_parser(
+        "version", help="version help"
+    )
+
+    version_generate_parser.add_argument(
+        "--user", "-u", help="The databus user", type=str
+    )
+
+    version_generate_parser.add_argument("--group", help="The group name", type=str)
+
+    version_generate_parser.add_argument(
+        "--artifact", help="The version artifact", type=str
+    )
+
+    version_generate_parser.add_argument("--versionid", help="The version id", type=str)
+
+    version_generate_parser.add_argument("--title", help="The version title", type=str)
+
+    version_generate_parser.add_argument(
+        "--publisher", help="The version publisher", type=str
+    )
+
+    version_generate_parser.add_argument(
+        "--comment", help="The version comment", type=str
+    )
+
+    version_generate_parser.add_argument(
+        "--doc", help="The version documentation", type=str
+    )
+
+    version_generate_parser.add_argument(
+        "--license", help="The version license", type=str
+    )
+
+    version_generate_parser.add_argument(
+        "URIs", nargs="+", help="The version license", type=str
+    )
+
+    # The parsers for direct deploy
+
+    direct_deploy_parser = subparsers.add_parser("deploy", help="deploy help")
+
+    direct_deploy_subparsers = direct_deploy_parser.add_subparsers(
+        dest='type',help="deploy group help"
+    )
+
+    direct_deploy_parser.add_argument(
+        "--apikey", help="Set the API key from your Databus Account", secure=True
+    )
+
+    # parser for deploying the group directly
+
+    direct_group_deploy_parser = direct_deploy_subparsers.add_parser("group")
+
+    direct_group_deploy_parser.add_argument("--user", help="The databus user", type=str)
+
+    direct_group_deploy_parser.add_argument("--group", help="The group name", type=str)
+
+    direct_group_deploy_parser.add_argument("--title", help="The group title", type=str)
+
+    direct_group_deploy_parser.add_argument(
+        "--comment", help="The group comment", type=str
+    )
+
+    direct_group_deploy_parser.add_argument(
+        "--doc", help="The group documentation", type=str
+    )
+
+    # parsers for deploying the version directly
+
+    direct_version_deploy_parser = direct_deploy_subparsers.add_parser("version")
+
+    direct_version_deploy_parser.add_argument(
+        "--user", help="The databus user", type=str
+    )
+
+    direct_version_deploy_parser.add_argument(
+        "--group", help="The group name", type=str
+    )
+
+    direct_version_deploy_parser.add_argument(
+        "--artifact", help="The version artifact", type=str
+    )
+
+    direct_version_deploy_parser.add_argument(
+        "--versionid", help="The version id", type=str
+    )
+
+    direct_version_deploy_parser.add_argument(
+        "--title", help="The version title", type=str
+    )
+
+    direct_version_deploy_parser.add_argument(
+        "--publisher", help="The version publisher", type=str
+    )
+
+    direct_version_deploy_parser.add_argument(
+        "--comment", help="The version comment", type=str
+    )
+
+    direct_version_deploy_parser.add_argument(
+        "--doc", help="The version documentation", type=str
+    )
+
+    direct_version_deploy_parser.add_argument(
+        "--license", help="The version license", type=str
+    )
+
+    direct_version_deploy_parser.add_argument(
+        "URIs", nargs="+", help="URIs to publish", type=str
+    )
+
+    print("DATABUS CLIENT v0.1")
+    print("-------------------")
+    print("Please insert the missing metadata:")
+
+    args = parser.parse_args()
+
+    if args.command == "generate":
+
+        if args.type == "group":
+            generate_group(args)
+        elif args.type == "version":
+            generate_version(args)
+        else:
+            print(f"Couldn't recognize type {args.type}")
+    elif args.command == "deploy":
+
+        if args.type == "group":
+            direct_group_deploy(args)
+        elif args.type == "version":
+            direct_version_deploy(args)
+        else:
+            print(f"Couldn't recognize type {args.type}")
+    else:
+        print(f"Couldn't recognize command {args.command}")
---
+++ python/databusclient/databus_client_lib.py
@@ -0,0 +1,188 @@
+import requests
+import json
+import hashlib
+import sys
+from datetime import datetime
+from dataclasses import dataclass, field
+from typing import List
+import argparse_prompt
+import logging
+
+@dataclass
+class DataGroup:
+    account_name: str
+    id: str
+    label: str
+    title: str
+    comment: str
+    abstract: str
+    description: str
+    DATABUS_BASE: str = "https://dev.databus.dbpedia.org"
+    context: str = "https://raw.githubusercontent.com/dbpedia/databus-git-mockup/main/dev/context.jsonld"
+
+    def get_target_uri(self) -> str:
+
+        return f"{self.DATABUS_BASE}/{self.account_name}/{self.id}"
+
+    def to_jsonld(self, **kwargs) -> str:
+        """Generates the json representation of group documentation"""
+
+        group_uri = f"{self.DATABUS_BASE}/{self.account_name}/{self.id}"
+
+        group_data_dict = {
+            "@context": self.context,
+            "@graph": [
+                {
+                    "@id": group_uri,
+                    "@type": "dataid:Group",
+                    "label": {"@value": self.label, "@language": "en"},
+                    "title": {"@value": self.title, "@language": "en"},
+                    "comment": {"@value": self.comment, "@language": "en"},
+                    "abstract": {"@value": self.abstract, "@language": "en"},
+                    "description": {"@value": self.description, "@language": "en"},
+                }
+            ],
+        }
+        return json.dumps(group_data_dict, **kwargs)
+
+
+class DatabusFile:
+    def __init__(self, uri: str, cvs: dict, file_ext: str, verbose=False, **kwargs):
+        """Fetches the necessary information of a file URI for the deploy to the databus."""
+        self.uri = uri
+        self.cvs = cvs
+        resp = requests.get(uri, **kwargs)
+
+        if verbose:
+            print()
+        if resp.status_code > 400:
+            print(f"ERROR for {uri} -> Status {str(resp.status_code)}")
+
+        self.sha256sum = hashlib.sha256(bytes(resp.content)).hexdigest()
+        self.content_length = str(len(resp.content))
+        self.file_ext = file_ext
+        self.id_string = "_".join([f"{k}={v}" for k, v in cvs.items()]) + "." + file_ext
+
+
+@dataclass
+class DataVersion:
+    account_name: str
+    group: str
+    artifact: str
+    version: str
+    title: str
+    label: str
+    publisher: str
+    comment: str
+    abstract: str
+    description: str
+    license: str
+    databus_files: List[DatabusFile]
+    issued: datetime = field(default_factory=datetime.now)
+    DATABUS_BASE: str = "https://dev.databus.dbpedia.org"
+    context: str = "https://raw.githubusercontent.com/dbpedia/databus-git-mockup/main/dev/context.jsonld"
+
+    def get_target_uri(self):
+
+        return f"https://dev.databus.dbpedia.org/{self.account_name}/{self.group}/{self.artifact}/{self.version}"
+
+    def __distinct_cvs(self) -> dict:
+
+        distinct_cv_definitions = {}
+        for dbfile in self.databus_files:
+            for key, value in dbfile.cvs.items():
+
+                if not key in distinct_cv_definitions:
+                    distinct_cv_definitions[key] = {
+                        "@type": "rdf:Property",
+                        "@id": f"dataid-cv:{key}",
+                        "rdfs:subPropertyOf": {"@id": "dataid:contentVariant"},
+                    }
+        return distinct_cv_definitions
+
+    def __dbfiles_to_dict(self):
+
+        for dbfile in self.databus_files:
+            file_dst = {
+                "@id": self.version_uri + "#" + dbfile.id_string,
+                "file": self.version_uri + "/" + self.artifact + "_" + dbfile.id_string,
+                "@type": "dataid:SingleFile",
+                "formatExtension": dbfile.file_ext,
+                "compression": "none",
+                "downloadURL": dbfile.uri,
+                "byteSize": dbfile.content_length,
+                "sha256sum": dbfile.sha256sum,
+                "hasVersion": self.version,
+            }
+            for key, value in dbfile.cvs.items():
+
+                file_dst[f"dataid-cv:{key}"] = value
+
+            yield file_dst
+
+    def to_jsonld(self, **kwargs) -> str:
+        self.version_uri = f"{self.DATABUS_BASE}/{account_name}/{group}/{artifact}/{version}"
+        self.data_id_uri = self.version_uri + "#Dataset"
+
+        self.artifact_uri = f"{self.DATABUS_BASE}/{account_name}/{group}/{artifact}"
+
+        self.group_uri = f"{self.DATABUS_BASE}/{account_name}/{group}"
+
+        self.timestamp = self.issued.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+        data_id_dict = {
+            "@context": self.context,
+            "@graph": [
+                {
+                    "@type": "dataid:Dataset",
+                    "@id": self.data_id_uri,
+                    "version": self.version_uri,
+                    "artifact": self.artifact_uri,
+                    "group": self.group_uri,
+                    "hasVersion": self.version,
+                    "issued": self.timestamp,
+                    "publisher": self.publisher,
+                    "label": {"@value": self.label, "@language": "en"},
+                    "title": {"@value": self.title, "@language": "en"},
+                    "comment": {"@value": self.comment, "@language": "en"},
+                    "abstract": {"@value": self.abstract, "@language": "en"},
+                    "description": {"@value": self.description, "@language": "en"},
+                    "license": {"@id": self.license},
+                    "distribution": [d for d in self.__dbfiles_to_dict()],
+                }
+            ],
+        }
+
+        for _, named_cv_prop in self.__distinct_cvs().items():
+            data_id_dict["@graph"].append(named_cv_prop)
+
+        # add explicit artifact statement
+
+        data_id_dict["@graph"].append(
+            {"@id": self.get_target_uri().rsplit("/", 1)[0], "@type": "dataid:Artifact"}
+        )
+
+        # Explicit Version Statement
+        data_id_dict["@graph"].append(
+            {"@id": self.get_target_uri(), "@type": "dataid:Version"}
+        )
+
+        return json.dumps(data_id_dict)
+
+
+def deploy_to_dev_databus(api_key: str, *databus_objects):
+
+    for dbobj in databus_objects:
+        print(f"Deploying {dbobj.get_target_uri()}")
+        submission_data = dbobj.to_jsonld()
+
+        resp = requests.put(
+            dbobj.get_target_uri(),
+            headers={"X-API-Key": api_key, "Content-Type": "application/json"},
+            data=submission_data,
+        )
+
+        if resp.status_code >= 400:
+            print(f"Response: Status {resp.status_code}; Text: {resp.text}")
+
+            print(f"Problematic file:\n {submission_data}")
