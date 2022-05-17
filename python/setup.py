3288442945089c7d066c4c1b8075d73de391c390
Marvin Hofer <mh68feta@studserv.uni-leipzig.de>
2022-05-17 17:16:04 +0200

    added py d8client deploy and test-deploy

--- python/setup.py
+++ python/setup.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python3
 
-
+import pathlib
 from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
 
 setup(name='databusclient',
       version='0.1',
       description='A simple client for submitting data to the databus',
+      long_description=README,
+      long_description_content_type="text/markdown",
       url='https://github.com/dbpedia/databus-client/python',
       author='DBpedia Association',
       author_email='',
