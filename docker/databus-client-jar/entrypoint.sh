1ea4cffc77178b3f9e39229127b88f89c895157f
Eisenbahnplatte <18533014+Eisenbahnplatte@users.noreply.github.com>
2023-06-26 17:54:35 +0200

    added docker with jar file

---
+++ docker/databus-client-jar/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+TARGET="/var/repo"
+
+args="--target $TARGET"
+
+if { [ -z "$SOURCE" ] && [ ! -z "$S" ]; } || { [ ! -z "$SOURCE" ] && [ -z "$S" ]; }; then
+    args="$args --source $SOURCE$S"
+fi
+
+if { [ -z "$ENDPOINT" ] && [ ! -z "$E" ]; } || { [ ! -z "$ENDPOINT" ] && [ -z "$E" ]; }; then
+    args="$args --endpoint $ENDPOINT$E"
+fi
+
+if { [ -z "$COMPRESSION" ] && [ ! -z "$C" ]; } || { [ ! -z "$COMPRESSION" ] && [ -z "$C" ]; }; then
+    args="$args --compression $COMPRESSION$C"
+fi
+
+if { [ -z "$FORMAT" ] && [ ! -z "$F" ]; } || { [ ! -z "$FORMAT" ] && [ -z "$F" ]; }; then
+    args="$args --format $FORMAT$F"
+fi
+
+
+java -jar databus-client.jar $args
